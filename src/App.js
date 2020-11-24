8a6fd66e0e5da04873e7fde5fc09def2f55b1e02
Victor Bolaños Solís <viktorb.132@gmail.com>
2020-11-24 17:58:56 -0600

    componentes y estados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// function HelloWorld(props) {
+//   console.log(props);
+//   return(
+//     <div id="hello">
+//       <h3>{ props.subtitle }</h3>
+//       { props.mytext }
+//     </div>
+//   );
+// }
+
+class HelloWorld extends React.Component {
+  state = {
+    show: true
+  }
+
+  toggleShow =() => {
+    this.setState({show: !this.state.show})
+  }
+  
+  render() {
+    if (this.state.show) {
+      return(
+        <div id="hello">
+          <h3>{ this.props.subtitle }</h3>
+          { this.props.mytext }
+          <button onClick={ this.toggleShow }>Toggle show</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h4>No elements</h4>
+          <button onClick={ this.toggleShow }>Toggle show</button>
+        </div>
+      );
+    }  
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>App component: 
+      <HelloWorld mytext="Hello Victor" subtitle="1"/>  
+      <HelloWorld mytext="Hello world" subtitle="2"/> 
+      <HelloWorld mytext="Hello!!" subtitle="3"/>
     </div>
   );
 }
